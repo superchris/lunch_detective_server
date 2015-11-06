14ae2465e9a56e98872da9b193e6f393c0d8f099
Chris Nelson <superchrisnelson@gmail.com>
2015-11-06 16:56:22 -0500

    herokuing better

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,6 @@ use Mix.Config
 config :lunch_detective_server, LunchDetectiveServer.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "lunch-detective-server.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
